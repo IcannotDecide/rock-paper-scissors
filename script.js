6d4c5c676b46d50822a700d607e0149d36d899a3
IcannotDecide <matthewat43@gmail.com>
2023-06-24 14:31:06 +0100

    Reformat script.js and added a comment

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const getComputerChoice =
     (Math.floor(Math.random() * 3) + 1) === 1 ? "rock"
-    : (Math.floor(Math.random() * 3) + 1) === 2 ? "paper"
-    : "scissors";
+        : (Math.floor(Math.random() * 3) + 1) === 2 ? "paper"
+            : "scissors";
 
 
 const playerSelection = prompt("Choose rock, paper or scissors").toLowerCase();
@@ -9,6 +9,9 @@ const playerSelection = prompt("Choose rock, paper or scissors").toLowerCase();
 
 function playRound(playerSelection, computerSelection) {
     console.log(playerSelection, computerSelection);
+
+
+// Looks at player selection and computer selection and finds out who one and console logs it
     if (playerSelection === "rock") {
         if (computerSelection === "rock") {
             console.log("You tie, computer chose rock")
@@ -24,17 +27,19 @@ function playRound(playerSelection, computerSelection) {
             console.log("You tie, computer chose paper")
         } else {
             console.log("You lose, computer chose scissors")
-        }  
+        }
+    } else if (playerSelection === "scissors") {
+        if (computerSelection === "rock") {
+            console.log("You lose, computer chose rock")
+        } else if (computerSelection === "paper") {
+            console.log("You win, computer chose paper")
+        } else {
+            console.log("You tie, computer chose scissors")
+        }
     } else {
-    if (computerSelection === "rock") {
-        console.log("You lose, computer chose rock")
-    } else if (computerSelection === "paper") {
-        console.log("You win, computer chose paper")
-    } else {
-        console.log("You tie, computer chose scissors")
+        console.log("Please choose between rock, paper or scissors")
     }
-}
-    
+
 };
 
 playRound(playerSelection, getComputerChoice)
